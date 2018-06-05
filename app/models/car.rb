ea92b31f8530d03403599982803ab859bba0bcd2
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-05 15:58:09 -0600

    initial commit of cars app

---
+++ app/models/car.rb
@@ -0,0 +1,19 @@
+class Car < ApplicationRecord
+  def self.by_model
+  end
+
+  def self.by_make
+  end
+
+  def self.by_price(high = false)
+  end
+
+  def paint(color)
+  end
+
+  def info
+  end
+
+  def honk
+  end
+end
---
+++ spec/models/car_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Car, type: :model do
+  describe 'attributes' do
+    it { should respond_to :make }
+  end
+end
+
+RSpec.describe Car, type: :model do
+  describe 'attributes' do
+    it 'has a make' do
+      make = 'Toyota'
+      car = Car.create(make: make)
+      expect(car.make).to eq(make)
+    end
+  end
+end
